17bea30703c1cc907550a34485c3ddbfaae9a9f3
Pelikh Evgeniy <pelikh123.evg@gmail.com>
2022-11-25 23:26:54 +0100

    ISPR-SITE-2022

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const linkButtonInMainMenu = document.querySelectorAll(".main__page a");
+
+linkButtonInMainMenu.forEach(function (items) {
+    items.style.borderRadius = "5px";
+});
+
